aedc983285eee967d29c73ae704070353b0fbb97
nevfeldeniz <nevfeldenizz@gmail.com>
2026-06-28 07:03:45 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         <Hero />
         <About />
         <Timeline />
-        <Skills />
         <Projects />
+        <Skills />
         <Contact />
       </main>
     </>
